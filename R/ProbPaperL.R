e965b398884f86f8d45c631d303f191b7afb6433
Shinsuke <sakai@fml.t.u-tokyo.ac.jp>
2018-10-22 11:07:00 +0900

    指数分布の追加

--- R/ProbPaperL.R
+++ R/ProbPaperL.R
@@ -34,3 +34,35 @@ PgumbelL <- function( Variable,Xlabel="Observed Value",MainTitle="Gumbel Probabi
   }
   legend("topleft",names,pch=pchs)
 }
+# 正規確率紙に累積相対度数をプロットする
+PnormL <- function(Variable,Xlabel="Observed Value",MainTitle="Normal Probability Paper",                            # データベクトル
+                  color="gray",names=c("1","2","3"),pchs=c(0,1,2))          # データベクトル
+{
+  Lnum<-length(Variable) #number of label elements
+  x<-NULL
+  nmax<-0
+  for(i in 1:Lnum){
+    x <- c(x,Variable[[i]])
+    nn<-length(Variable[[i]])
+    if(nn>nmax)nmax<-nn
+  }
+  x <- x[!is.na(x)]
+  xmin<-min(x); xmax<-max(x)
+  probs <- c(0.01, 0.1, 1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99, 99.9, 99.99)/100
+  plot(c(xmin, xmax), qnorm(c(probs[1], probs[17])), type="n", yaxt="n",
+       xlab=Xlabel, ylab="Cumulative Percent",
+       main=MainTitle)
+  abline(h=qnorm(probs), col=color)                     # 水平の格子線
+  abline(v=axTicks(1),col=color)             # 垂直の格子線axTicksは目盛の値を得る関数
+#  points(x, qnorm(y))
+  axis(2, qnorm(probs), probs*100)
+  for(i in 1:Lnum){
+    x <- Variable[[i]]
+    x <- x[!is.na(x)]                            # 欠損値を除く
+    n <- length(x)                                       # 有効データ数
+    y <- qnorm(1:n/(n+1))                         # 正規分布における累積密度の位置(ミーンランク)
+    x <- sort(x)                                 # 昇順に並べ替える
+    points(x, y,pch=pchs[i])                                #データ点を描く
+  }
+  legend("topleft",names,pch=pchs)
+}
